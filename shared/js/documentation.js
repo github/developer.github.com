0e4a63014497798edcba52e338557854c24edf77
Tim Clem <timothy.clem@gmail.com>
2011-05-19 15:19:44 -0700

    Major update, new styles, added user api docs.
    - Styles redone through-out
    - User api documented
    - A few new deprecation warnings
    - Add issue comments into nav
    - Lots of other misc cleanup and fixes

---
+++ shared/js/documentation.js
@@ -0,0 +1,43 @@
+// Init sidebar
+$(function() {
+  var activeItem,
+      helpList = $('#js-help-sidebar .js-topic'),
+      firstOccurance = true
+
+  $('#js-help-sidebar .js-guides').each(function(){
+    if($(this).find('.disable').length == 0 || firstOccurance == false){
+      $(this).children().hide()
+    } else {
+      activeItem = $(this).index()
+      firstOccurance = false
+    }
+  })
+
+  $('#js-help-sidebar .js-topic h3 a').click(function(){
+    var clickedTopic = $(this).parents('.js-topic'),
+        topicGuides = clickedTopic.find('.js-guides li')
+    
+    if(activeItem != clickedTopic.index()){
+      if(helpList.eq(activeItem)){
+        helpList.eq(activeItem).find('.js-guides li').toggle(100)
+      }
+      activeItem = clickedTopic.index()
+      topicGuides.toggle(100)
+    } else {
+      activeItem = undefined
+      topicGuides.toggle(100)
+    }
+
+    return false
+  })
+
+  $('.help-search .search-box').focus(function(){
+    $(this).css('background-position','0px -25px')
+  })
+
+  $('.help-search .search-box').focusout(function(){
+    if($(this).val() == ''){
+      $(this).css('background-position','0px 0px')
+    }
+  })
+});

8eb8c11fe0f0dbdd3e4769ae0d3c091ab4266466
Cameron McEfee <cameron@github.com>
2011-06-30 11:43:11 -0700

    boom

--- shared/js/documentation.js
+++ shared/js/documentation.js
@@ -4,8 +4,26 @@ $(function() {
       helpList = $('#js-help-sidebar .js-topic'),
       firstOccurance = true
 
-  $('#js-help-sidebar .js-topic').each(function(){
-    if($(this).find('.disable').length == 0 || firstOccurance == false){
+  // hide list items at startup
+  if($('body.api') && window.location){
+    var reg = /\/\/[^\/]+(\/.+)/g,
+        docUrl = reg.exec(window.location.toString())
+    if(docUrl){
+      $('#js-sidebar .js-topic a').each(function(){
+        var url = $(this).attr('href').toString()
+        if(url.indexOf(docUrl[1]) >= 0 && url.length == docUrl[1].length){
+          $(this).parent('li').addClass('disable')
+          var parentTopic = $(this).parentsUntil('div.sidebar-module > ul').last()
+          parentTopic.addClass('js-current')
+          parentTopic.find('.js-expand-btn').toggleClass('collapsed expanded')
+        }
+      })
+    }
+  }
+
+  $('#js-sidebar .js-topic').each(function(){
+    if(($(this).find('.disable').length == 0 || firstOccurance == false) &&
+    $(this).hasClass('js-current') != true){
       $(this).find('.js-guides').children().hide()
     } else {
       activeItem = $(this).index()
@@ -13,7 +31,19 @@ $(function() {
     }
   })
 
-  $('#js-help-sidebar .js-topic h3 a').click(function(){
+  // Toggle style list. Expanded items stay
+  // expanded when new items are clicked.
+  $('#js-sidebar .js-toggle-list .js-expand-btn').click(function(){
+    var clickedTopic = $(this).parents('.js-topic'),
+        topicGuides  = clickedTopic.find('.js-guides li')
+    $(this).toggleClass('collapsed expanded')
+    topicGuides.toggle(100)
+    return false
+  })
+
+  // Accordion style list. Expanded items
+  // collapse when new items are clicked.
+  $('#js-sidebar .js-accordion-list .js-topic h3 a').click(function(){
     var clickedTopic = $(this).parents('.js-topic'),
         topicGuides = clickedTopic.find('.js-guides li')
     

8499a9218239437097f1a57a8b153aeca8c16331
Wynn Netherland <wynn@github.com>
2013-01-03 13:48:25 -0600

    Add status to header

--- static/shared/js/documentation.js
+++ static/shared/js/documentation.js
@@ -76,4 +76,20 @@ $(function() {
   var currentYear = (new Date).getFullYear();
   $("#year").text( (new Date).getFullYear() );
 
+  // Grab API status
+  $.getJSON('https://status.github.com/api/status.json?callback=?', function(data) {
+    if(data) {
+      var link = $("<a>")
+        .attr("href", "https://status.github.com")
+        .addClass(data.status)
+        .attr("title", "API Status: " + data.status + ". Click for details.");
+      var img = $("<img>")
+        .attr("src", "/images/status-icon-" + data.status + ".png")
+        .height(16)
+        .width(16);
+      link.append(img);
+      $('.api-status').html(link);
+    }
+  });
+
 });

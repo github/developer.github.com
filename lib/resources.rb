9bb528024ce46bef150d7242d22be5b14b990038
Jason Rudolph <github@jasonrudolph.com>
2013-04-11 16:56:32 -0400

    Add url attributes to readme content hash

--- lib/resources.rb
+++ lib/resources.rb
@@ -997,7 +997,10 @@ module GitHub
       "name" =>  "README.md",
       "path" =>  "README.md",
       "content" =>  "encoded content ...",
-      "sha" =>  "3d21ec53a331a6f037a91c368710b99387d012c1"
+      "sha" =>  "3d21ec53a331a6f037a91c368710b99387d012c1",
+      "url" => "https://api.github.com/repos/pengwynn/octokit/contents/README.md",
+      "git_url" => "https://api.github.com/repos/pengwynn/octokit/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1",
+      "html_url" => "https://github.com/pengwynn/octokit/blob/master/README.md",
     }
 
     STATUS = {

ffe8cbf5e5ce9d7587b64a075ffbde5b62446bd7
Jason Rudolph <github@jasonrudolph.com>
2013-04-11 17:05:21 -0400

    Move _links to bottom of hash to deemphasize them

--- lib/resources.rb
+++ lib/resources.rb
@@ -988,11 +988,6 @@ module GitHub
     README_CONTENT = {
       "type" =>  "file",
       "encoding" =>  "base64",
-      "_links" =>  {
-        "git" =>  "https://api.github.com/repos/pengwynn/octokit/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1",
-        "self" =>  "https://api.github.com/repos/pengwynn/octokit/contents/README.md",
-        "html" =>  "https://github.com/pengwynn/octokit/blob/master/README.md"
-      },
       "size" =>  5362,
       "name" =>  "README.md",
       "path" =>  "README.md",
@@ -1001,6 +996,11 @@ module GitHub
       "url" => "https://api.github.com/repos/pengwynn/octokit/contents/README.md",
       "git_url" => "https://api.github.com/repos/pengwynn/octokit/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1",
       "html_url" => "https://github.com/pengwynn/octokit/blob/master/README.md",
+      "_links" =>  {
+        "git" =>  "https://api.github.com/repos/pengwynn/octokit/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1",
+        "self" =>  "https://api.github.com/repos/pengwynn/octokit/contents/README.md",
+        "html" =>  "https://github.com/pengwynn/octokit/blob/master/README.md"
+      },
     }
 
     STATUS = {

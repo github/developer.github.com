3f41d9b477995399e8b51bf37e08145809c1d38e
Jason Rudolph <github@jasonrudolph.com>
2013-04-24 18:19:58 -0600

    Announce deprecation of merge_commit_sha

--- lib/resources.rb
+++ lib/resources.rb
@@ -31,7 +31,8 @@ module GitHub
         :pengwynn     => '7e19cd5486b5d6dc1ef90e671ba52ae0',
         :pezra        => 'f38112009dc16547051c8ac246cee443',
         :rick         => 'a44d5abad6e86cff4e34d9f0839535c9',
-        :agh          => '6af915d3c6aa4ad30bbad43d8035fe10'
+        :agh          => '6af915d3c6aa4ad30bbad43d8035fe10',
+        :jasonrudolph => '592e1e6f041f9a4ec51846fd82013aea'
       }
 
       DefaultTimeFormat = "%B %-d, %Y".freeze

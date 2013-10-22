2c3c279650bdf8ef7c3a67cd12614e8d36418da0
Jason Rudolph <github@jasonrudolph.com>
2013-10-22 08:40:45 -0400

    Update code search example
    Update example to reflect the new validation rule for the Code Search API
    (described in 351f716).

--- lib/resources.rb
+++ lib/resources.rb
@@ -892,78 +892,79 @@ module GitHub
     }
 
     CODE_SEARCH_V3_RESULTS = {
-      "total_count" => 104,
+      "total_count" => 7,
       "items" => [
         {
-          "name" => "github-issue-importer.gemspec",
-          "path" => "github-issue-importer.gemspec",
-          "sha" => "394508202991504d8a0771ae027454facaaa045a",
-          "url" => "https://api.github.com/repositories/1586630/contents/github-issue-importer.gemspec?ref=aa22a4be513163c73531e96bd99f4b49d6ded8a6",
-          "git_url" => "https://api.github.com/repositories/1586630/git/blobs/394508202991504d8a0771ae027454facaaa045a",
-          "html_url" => "https://github.com/johnf/github-issue-importer/blob/aa22a4be513163c73531e96bd99f4b49d6ded8a6/github-issue-importer.gemspec",
+          "name" => "classes.js",
+          "path" => "src/attributes/classes.js",
+          "sha" => "d7212f9dee2dcc18f084d7df8f417b80846ded5a",
+          "url" => "https://api.github.com/repositories/167174/contents/src/attributes/classes.js?ref=825ac3773694e0cd23ee74895fd5aeb535b27da4",
+          "git_url" => "https://api.github.com/repositories/167174/git/blobs/d7212f9dee2dcc18f084d7df8f417b80846ded5a",
+          "html_url" => "https://github.com/jquery/jquery/blob/825ac3773694e0cd23ee74895fd5aeb535b27da4/src/attributes/classes.js",
           "repository" => {
-            "id" => 1586630,
-            "name" => "github-issue-importer",
-            "full_name" => "johnf/github-issue-importer",
+            "id" => 167174,
+            "name" => "jquery",
+            "full_name" => "jquery/jquery",
             "owner" => {
-              "login" => "johnf",
-              "id" => 42590,
-              "avatar_url" => "https://secure.gravatar.com/avatar/ab4d879ba3233a270aa14f447c795505?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
-              "gravatar_id" => "ab4d879ba3233a270aa14f447c795505",
-              "url" => "https://api.github.com/users/johnf",
-              "html_url" => "https://github.com/johnf",
-              "followers_url" => "https://api.github.com/users/johnf/followers",
-              "following_url" => "https://api.github.com/users/johnf/following{/other_user}",
-              "gists_url" => "https://api.github.com/users/johnf/gists{/gist_id}",
-              "starred_url" => "https://api.github.com/users/johnf/starred{/owner}{/repo}",
-              "subscriptions_url" => "https://api.github.com/users/johnf/subscriptions",
-              "organizations_url" => "https://api.github.com/users/johnf/orgs",
-              "repos_url" => "https://api.github.com/users/johnf/repos",
-              "events_url" => "https://api.github.com/users/johnf/events{/privacy}",
-              "received_events_url" => "https://api.github.com/users/johnf/received_events",
-              "type" => "User"
+              "login" => "jquery",
+              "id" => 70142,
+              "avatar_url" => "https://0.gravatar.com/avatar/6906f317a4733f4379b06c32229ef02f?d=https%3A%2F%2Fidenticons.github.com%2Ff426f04f2f9813718fb806b30e0093de.png",
+              "gravatar_id" => "6906f317a4733f4379b06c32229ef02f",
+              "url" => "https://api.github.com/users/jquery",
+              "html_url" => "https://github.com/jquery",
+              "followers_url" => "https://api.github.com/users/jquery/followers",
+              "following_url" => "https://api.github.com/users/jquery/following{/other_user}",
+              "gists_url" => "https://api.github.com/users/jquery/gists{/gist_id}",
+              "starred_url" => "https://api.github.com/users/jquery/starred{/owner}{/repo}",
+              "subscriptions_url" => "https://api.github.com/users/jquery/subscriptions",
+              "organizations_url" => "https://api.github.com/users/jquery/orgs",
+              "repos_url" => "https://api.github.com/users/jquery/repos",
+              "events_url" => "https://api.github.com/users/jquery/events{/privacy}",
+              "received_events_url" => "https://api.github.com/users/jquery/received_events",
+              "type" => "Organization",
+              "site_admin" => false
             },
             "private" => false,
-            "html_url" => "https://github.com/johnf/github-issue-importer",
-            "description" => "Import Issues from Launchpad (for now) into github",
+            "html_url" => "https://github.com/jquery/jquery",
+            "description" => "jQuery JavaScript Library",
             "fork" => false,
-            "url" => "https://api.github.com/repos/johnf/github-issue-importer",
-            "forks_url" => "https://api.github.com/repos/johnf/github-issue-importer/forks",
-            "keys_url" => "https://api.github.com/repos/johnf/github-issue-importer/keys{/key_id}",
-            "collaborators_url" => "https://api.github.com/repos/johnf/github-issue-importer/collaborators{/collaborator}",
-            "teams_url" => "https://api.github.com/repos/johnf/github-issue-importer/teams",
-            "hooks_url" => "https://api.github.com/repos/johnf/github-issue-importer/hooks",
-            "issue_events_url" => "https://api.github.com/repos/johnf/github-issue-importer/issues/events{/number}",
-            "events_url" => "https://api.github.com/repos/johnf/github-issue-importer/events",
-            "assignees_url" => "https://api.github.com/repos/johnf/github-issue-importer/assignees{/user}",
-            "branches_url" => "https://api.github.com/repos/johnf/github-issue-importer/branches{/branch}",
-            "tags_url" => "https://api.github.com/repos/johnf/github-issue-importer/tags",
-            "blobs_url" => "https://api.github.com/repos/johnf/github-issue-importer/git/blobs{/sha}",
-            "git_tags_url" => "https://api.github.com/repos/johnf/github-issue-importer/git/tags{/sha}",
-            "git_refs_url" => "https://api.github.com/repos/johnf/github-issue-importer/git/refs{/sha}",
-            "trees_url" => "https://api.github.com/repos/johnf/github-issue-importer/git/trees{/sha}",
-            "statuses_url" => "https://api.github.com/repos/johnf/github-issue-importer/statuses/{sha}",
-            "languages_url" => "https://api.github.com/repos/johnf/github-issue-importer/languages",
-            "stargazers_url" => "https://api.github.com/repos/johnf/github-issue-importer/stargazers",
-            "contributors_url" => "https://api.github.com/repos/johnf/github-issue-importer/contributors",
-            "subscribers_url" => "https://api.github.com/repos/johnf/github-issue-importer/subscribers",
-            "subscription_url" => "https://api.github.com/repos/johnf/github-issue-importer/subscription",
-            "commits_url" => "https://api.github.com/repos/johnf/github-issue-importer/commits{/sha}",
-            "git_commits_url" => "https://api.github.com/repos/johnf/github-issue-importer/git/commits{/sha}",
-            "comments_url" => "https://api.github.com/repos/johnf/github-issue-importer/comments{/number}",
-            "issue_comment_url" => "https://api.github.com/repos/johnf/github-issue-importer/issues/comments/{number}",
-            "contents_url" => "https://api.github.com/repos/johnf/github-issue-importer/contents/{+path}",
-            "compare_url" => "https://api.github.com/repos/johnf/github-issue-importer/compare/{base}...{head}",
-            "merges_url" => "https://api.github.com/repos/johnf/github-issue-importer/merges",
-            "archive_url" => "https://api.github.com/repos/johnf/github-issue-importer/{archive_format}{/ref}",
-            "downloads_url" => "https://api.github.com/repos/johnf/github-issue-importer/downloads",
-            "issues_url" => "https://api.github.com/repos/johnf/github-issue-importer/issues{/number}",
-            "pulls_url" => "https://api.github.com/repos/johnf/github-issue-importer/pulls{/number}",
-            "milestones_url" => "https://api.github.com/repos/johnf/github-issue-importer/milestones{/number}",
-            "notifications_url" => "https://api.github.com/repos/johnf/github-issue-importer/notifications{?since,all,participating}",
-            "labels_url" => "https://api.github.com/repos/johnf/github-issue-importer/labels{/name}"
+            "url" => "https://api.github.com/repos/jquery/jquery",
+            "forks_url" => "https://api.github.com/repos/jquery/jquery/forks",
+            "keys_url" => "https://api.github.com/repos/jquery/jquery/keys{/key_id}",
+            "collaborators_url" => "https://api.github.com/repos/jquery/jquery/collaborators{/collaborator}",
+            "teams_url" => "https://api.github.com/repos/jquery/jquery/teams",
+            "hooks_url" => "https://api.github.com/repos/jquery/jquery/hooks",
+            "issue_events_url" => "https://api.github.com/repos/jquery/jquery/issues/events{/number}",
+            "events_url" => "https://api.github.com/repos/jquery/jquery/events",
+            "assignees_url" => "https://api.github.com/repos/jquery/jquery/assignees{/user}",
+            "branches_url" => "https://api.github.com/repos/jquery/jquery/branches{/branch}",
+            "tags_url" => "https://api.github.com/repos/jquery/jquery/tags",
+            "blobs_url" => "https://api.github.com/repos/jquery/jquery/git/blobs{/sha}",
+            "git_tags_url" => "https://api.github.com/repos/jquery/jquery/git/tags{/sha}",
+            "git_refs_url" => "https://api.github.com/repos/jquery/jquery/git/refs{/sha}",
+            "trees_url" => "https://api.github.com/repos/jquery/jquery/git/trees{/sha}",
+            "statuses_url" => "https://api.github.com/repos/jquery/jquery/statuses/{sha}",
+            "languages_url" => "https://api.github.com/repos/jquery/jquery/languages",
+            "stargazers_url" => "https://api.github.com/repos/jquery/jquery/stargazers",
+            "contributors_url" => "https://api.github.com/repos/jquery/jquery/contributors",
+            "subscribers_url" => "https://api.github.com/repos/jquery/jquery/subscribers",
+            "subscription_url" => "https://api.github.com/repos/jquery/jquery/subscription",
+            "commits_url" => "https://api.github.com/repos/jquery/jquery/commits{/sha}",
+            "git_commits_url" => "https://api.github.com/repos/jquery/jquery/git/commits{/sha}",
+            "comments_url" => "https://api.github.com/repos/jquery/jquery/comments{/number}",
+            "issue_comment_url" => "https://api.github.com/repos/jquery/jquery/issues/comments/{number}",
+            "contents_url" => "https://api.github.com/repos/jquery/jquery/contents/{+path}",
+            "compare_url" => "https://api.github.com/repos/jquery/jquery/compare/{base}...{head}",
+            "merges_url" => "https://api.github.com/repos/jquery/jquery/merges",
+            "archive_url" => "https://api.github.com/repos/jquery/jquery/{archive_format}{/ref}",
+            "downloads_url" => "https://api.github.com/repos/jquery/jquery/downloads",
+            "issues_url" => "https://api.github.com/repos/jquery/jquery/issues{/number}",
+            "pulls_url" => "https://api.github.com/repos/jquery/jquery/pulls{/number}",
+            "milestones_url" => "https://api.github.com/repos/jquery/jquery/milestones{/number}",
+            "notifications_url" => "https://api.github.com/repos/jquery/jquery/notifications{?since,all,participating}",
+            "labels_url" => "https://api.github.com/repos/jquery/jquery/labels{/name}"
           },
-          "score" => 0.96691436
+          "score" => 0.5269679,
         }
       ]
     }
@@ -971,45 +972,31 @@ module GitHub
     CODE_SEARCH_V3_RESULTS_HIGHLIGHTING = {
       "text_matches" => [
         {
-          "object_url" => "https://api.github.com/repositories/8460725/contents/.bundle/ruby/1.8/specifications/octokit-1.23.0.gemspec?ref=9c580b22493df1b3044c321729278c4e79302b7f",
+          "object_url" => "https://api.github.com/repositories/167174/contents/src/attributes/classes.js?ref=825ac3773694e0cd23ee74895fd5aeb535b27da4",
           "object_type" => "FileContent",
           "property" => "content",
-          "fragment" => "name = %q{octokit}\n  s.version = \"1.23.0\"\n\n  s.required_rubygems_version = Gem::Requirement.new(\">= 1.3.6\")",
+          "fragment" => ";\n\njQuery.fn.extend({\n\taddClass: function( value ) {\n\t\tvar classes, elem, cur, clazz, j, finalValue",
           "matches" => [
             {
-              "text" => "octokit",
+              "text" => "addClass",
               "indices" => [
-                10,
-                17
+                23,
+                31
               ]
             }
           ]
         },
         {
-          "object_url" => "https://api.github.com/repositories/8460725/contents/.bundle/ruby/1.8/specifications/octokit-1.23.0.gemspec?ref=9c580b22493df1b3044c321729278c4e79302b7f",
+          "object_url" => "https://api.github.com/repositories/167174/contents/src/attributes/classes.js?ref=825ac3773694e0cd23ee74895fd5aeb535b27da4",
           "object_type" => "FileContent",
           "property" => "content",
-          "fragment" => "\"Rakefile\", \"octokit.gemspec\", \"lib/faraday/response/raise_octokit_error.rb\", \"lib/octokit/authentication.rb\"",
+          "fragment" => ".isFunction( value ) ) {\n\t\t\treturn this.each(function( j ) {\n\t\t\t\tjQuery( this ).addClass( value.call( this",
           "matches" => [
             {
-              "text" => "octokit",
+              "text" => "addClass",
               "indices" => [
-                13,
-                20
-              ]
-            },
-            {
-              "text" => "octokit",
-              "indices" => [
-                59,
-                66
-              ]
-            },
-            {
-              "text" => "octokit",
-              "indices" => [
-                83,
-                90
+                80,
+                88
               ]
             }
           ]

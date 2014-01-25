481982a5d685439fbba0f0e0042d7cf1ef2639eb
Ivan Zuzak <izuzak@gmail.com>
2014-01-25 12:43:20 +0100

    document contents_url for files

--- lib/resources.rb
+++ lib/resources.rb
@@ -513,6 +513,7 @@ module GitHub
       "changes"   => 124,
       "blob_url"  => "https://github.com/octocat/Hello-World/blob/6dcb09b5b57875f334f61aebed695e2e4193db5e/file1.txt",
       "raw_url"   => "https://github.com/octocat/Hello-World/raw/6dcb09b5b57875f334f61aebed695e2e4193db5e/file1.txt",
+      "contents_url" => "https://api.github.com/repos/octocat/Hello-World/contents/file1.txt?ref=6dcb09b5b57875f334f61aebed695e2e4193db5e",
       "patch"     => "@@ -132,7 +132,7 @@ module Test @@ -1000,7 +1000,7 @@ module Test"
     }
 

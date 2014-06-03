d7a9c1c90d6cf33374a3868bb21d901bba794bef
Garen Torikian <gjtorikian@gmail.com>
2014-06-03 13:48:13 -0700

    Update settings JSON blob

--- lib/resources.rb
+++ lib/resources.rb
@@ -1927,45 +1927,115 @@ module GitHub
     ]
 
     FETCH_SETTINGS = {
-      "device" =>  {
-        "path" =>  "/dev/xyz"
+      "enterprise" => {
+        "private_mode" => false,
+        "github_hostname" => "ghe.local",
+        "auth_mode" => "default",
+        "storage_mode" => "rootfs",
+        "admin_password" => nil,
+        "configuration_id" => 1401777404,
+        "configuration_run_count" => 4,
+        "package_version" => "11.10.332",
+        "avatar" => {
+          "enabled" => false,
+          "uri" => ""
+        },
+        "customer" => {
+          "name" => "GitHub",
+          "email" => "lee@github.com",
+          "uuid" => "af6cac80-e4e1-012e-d822-1231380e52e9",
+          "secret_key_data" => "-----BEGIN PGP PRIVATE KEY BLOCK-----\nVersion: GnuPG v1.4.10 (GNU/Linux)\n\nlQcYBE5TCgsBEACk4yHpUcapplebaumBMXYMiLF+nCQ0lxpx...\n-----END PGP PRIVATE KEY BLOCK-----\n",
+          "public_key_data" => "-----BEGIN PGP PUBLIC KEY BLOCK-----\nVersion: GnuPG v1.4.10 (GNU/Linux)\n\nmI0ETqzZYgEEALSe6snowdenXyqvLfSQ34HWD6C7....\n-----END PGP PUBLIC KEY BLOCK-----\n"
+        },
+        "license" => {
+          "seats" => 0,
+          "evaluation" => false,
+          "expire_at" => "2015-04-27T00:00:00-07:00",
+          "perpetual" => false,
+          "unlimited_seating" => true,
+          "support_key" => "ssh-rsa AAAAB3N....",
+          "ssh_allowed" => true
+        },
+        "github_ssl" => {
+          "enabled" => false,
+          "cert" => nil,
+          "key" => nil
+        },
+        "ldap" => {
+          "host" => "",
+          "port" => "",
+          "base" => [
+
+          ],
+          "uid" => "",
+          "bind_dn" => "",
+          "password" => "",
+          "method" => "Plain",
+          "user_groups" => [
+
+          ],
+          "admin_group" => ""
+        },
+        "cas" => {
+          "url" => ""
+        },
+        "github_oauth" => {
+          "client_id" => "12313412",
+          "client_secret" => "kj123131132",
+          "organization_name" => "Homestar Runners",
+          "organization_team" => "homestarrunners/owners"
+        },
+        "smtp" => {
+          "enabled" => true,
+          "address" => "smtp.example.com",
+          "authentication" => "plain",
+          "port" => "1234",
+          "domain" => "blah",
+          "username" => "foo",
+          "user_name" => "mr_foo",
+          "enable_starttls_auto" => true,
+          "password" => "bar",
+          "support_address" => "enterprise@github.com",
+          "noreply_address" => "noreply@github.com"
+        },
+        "dns" => {
+          "primary_nameserver" => "8.8.8.8",
+          "secondary_nameserver" => "8.8.4.4"
+        },
+        "ntp" => {
+          "primary_server" => "0.ubuntu.pool.ntp.org",
+          "secondary_server" => "1.ubuntu.pool.ntp.org"
+        },
+        "timezone" => {
+          "identifier" => "UTC"
+        },
+        "device" => {
+          "path" => "/dev/xyz"
+        },
+        "snmp" => {
+          "enabled" => false,
+          "community" => ""
+        },
+        "rsyslog" => {
+          "enabled" => false,
+          "server" => "",
+          "protocol_name" => "TCP"
+        },
+        "pages" => {
+          "enabled" => true
+        },
+        "collectd" => {
+          "enabled" => false,
+          "server" => "",
+          "port" => "",
+          "encryption" => "",
+          "username" => "foo",
+          "password" => "bar"
+        }
       },
-      "smtp" =>  {
-        "enabled" =>  true,
-        "address" =>  "smtp.example.com",
-        "authentication" =>  "plain",
-        "port" =>  "1234",
-        "domain" =>  "blah",
-        "username" =>  "foo",
-        "password" =>  "bar",
-        "support_address" =>  "enterprise@github.com",
-        "noreply_address" =>  "noreply@github.com"
-      },
-      "dns" =>  {
-        "primary_nameserver" =>  "8.8.8.8"
-      },
-      "private_mode" =>  true,
-      "ldap" =>  {
-        "host" =>  "ldap.example.com",
-        "port" =>  389,
-        "base" =>  "ou=People,dc=github,dc=com",
-        "uid" =>  "github"
-      },
-      "github_hostname" =>  "github.example.com",
-      "github_ssl" =>  {
-        "enabled" =>  false
-      },
-      "storage_mode" =>  "rootfs",
-      "cas" =>  {
-        "url"  =>  "cas.example.com"
-      },
-      "auth_mode" =>  "default",
-      "snmp" =>  {
-        "enabled" =>  true
-      },
-      "rsyslog" =>  {
-        "enabled" =>  false
-      }
+      "run_list" => [
+        "role[configure]"
+      ]
     }
 
     CHECK_MAINTENANCE_STATUS = {

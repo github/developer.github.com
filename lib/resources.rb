39f3eb7573a949852d228e4962627dc9d39c7c0b
Ivan Žužak <ivan.zuzak@github.com>
2014-12-21 11:03:44 +0100

    Document missing attributes for repo hooks and org hooks

--- lib/resources.rb
+++ lib/resources.rb
@@ -1566,11 +1566,15 @@ Name | Type | Description
     HOOK ||= {
       "id" => 1,
       "url" => "https://api.github.com/repos/octocat/Hello-World/hooks/1",
+      "test_url" => "https://api.github.com/repos/octocat/Hello-World/hooks/1/test",
+      "ping_url" => "https://api.github.com/repos/octocat/Hello-World/hooks/1/pings",
       "name" => "web",
       "events" => ["push", "pull_request"],
       "active" => true,
       "config" =>
         {'url' => 'http://example.com', 'content_type' => 'json'},
+      "last_response" => 
+        {'code' => 200, 'status' => 'active', 'message' => 'OK'},
       "updated_at" => "2011-09-06T20:39:23Z",
       "created_at" => "2011-09-06T17:26:27Z",
     }
@@ -1578,6 +1582,7 @@ Name | Type | Description
     ORG_HOOK ||= {
       "id" => 1,
       "url" => "https://api.github.com/orgs/octocat/hooks/1",
+      "ping_url" => "https://api.github.com/orgs/octocat/hooks/1/pings",
       "name" => "web",
       "events" => ["push", "pull_request"],
       "active" => true,

53f40e74ea1465a446af10bfbe6eb17ff0a75d40
Garen Torikian <gjtorikian@gmail.com>
2015-01-12 14:47:57 -0800

    Move to using CSS classes for hiding enterprise-only content

--- lib/enterprise-purge.rb
+++ lib/enterprise-purge.rb
@@ -8,7 +8,7 @@ class EnterprisePurgeFilter < Nanoc::Filter
   # if we're running in Dotcom mode, we'll be lazy and just hide the content.
   # otherwise, when running script/enterprise-cutter, we'll bring these sections back
   def run(content, params={})
-    start_replacement = '<div class="enterprise-only" style="display: none">'
+    start_replacement = '<div class="enterprise-only">'
     end_replacement = '</div>'
 
     content = content.gsub(%r{<p>#{ENTERPRISE_START_STRING}</p>}, start_replacement)

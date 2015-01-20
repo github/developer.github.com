77aa6da70e8168c22cb300c273af347509ccf0bd
Ivan Zuzak <izuzak@gmail.com>
2015-01-20 17:59:25 +0100

    Use same webhook url as in example requests

--- lib/resources.rb
+++ lib/resources.rb
@@ -1582,7 +1582,7 @@ Name | Type | Description
       "events" => ["push", "pull_request"],
       "active" => true,
       "config" =>
-        {'url' => 'http://example.com', 'content_type' => 'json'},
+        {'url' => 'http://example.com/webhook', 'content_type' => 'json'},
       "updated_at" => "2011-09-06T20:39:23Z",
       "created_at" => "2011-09-06T17:26:27Z",
     }

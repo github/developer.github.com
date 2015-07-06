0a9da64534648881d7f69095504f0db10feaaec4
Garen Torikian <gjtorikian@gmail.com>
2015-07-06 16:32:02 -0700

    Add the Migrations API

--- lib/resources.rb
+++ lib/resources.rb
@@ -883,6 +883,17 @@ This endpoint may also return pull requests in the response. If an issue *is* a 
     ACTIVE_ORG_MEMBERSHIPS  ||= [ACTIVE_ADMIN_ORG_MEMBERSHIP]
     PENDING_ORG_MEMBERSHIPS ||= [PENDING_ADMIN_ORG_MEMBERSHIP]
 
+    MIGRATIONS ||= {
+      "id" => 79,
+      "guid" => "0b989ba4-242f-11e5-81e1-c7b6966d2516",
+      "state" => "pending",
+      "lock_repositories" => true,
+      "url" => "https://api.github.com/orgs/octo-org/migrations/79",
+      "created_at" => "2015-07-06T15:33:38-07:00",
+      "updated_at" => "2015-07-06T15:33:38-07:00",
+      "repositories" => [REPO]
+    }
+
     LABEL ||= {
       "url"   => "https://api.github.com/repos/octocat/Hello-World/labels/bug",
       "name"  => "bug",

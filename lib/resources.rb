996dd4e5ee8f45b09a12a5f9a453f14d7b682868
Hubot <cwanstrath+hubot@gmail.com>
2015-10-16 18:13:46 +0000

    Sync changes from upstream repository

--- lib/resources.rb
+++ lib/resources.rb
@@ -926,6 +926,7 @@ This endpoint may also return pull requests in the response. If an issue *is* a 
       "guid" => "0b989ba4-242f-11e5-81e1-c7b6966d2516",
       "state" => "pending",
       "lock_repositories" => true,
+      "exclude_attachments" => false,
       "url" => "https://api.github.com/orgs/octo-org/migrations/79",
       "created_at" => "2015-07-06T15:33:38-07:00",
       "updated_at" => "2015-07-06T15:33:38-07:00",

c5df1ce6c0663450484f05fc618f7e0f0fa9f7e8
Hubot <cwanstrath+hubot@gmail.com>
2016-04-21 07:34:35 -0700

    Sync changes from upstream repository

--- lib/responses/oauth.rb
+++ lib/responses/oauth.rb
@@ -23,6 +23,19 @@ module GitHub
       }
 
       OAUTH_ACCESS_WITH_USER ||= OAUTH_ACCESS.merge(:user => USER)
+
+      OAUTH_AUTHORIZATION ||= {
+        "id" => 1,
+        "url" => "https://api.github.com/applications/grants/1",
+        "app" => {
+          "url" => "http://my-github-app.com",
+          "name" => "my github app",
+          "client_id" => "abcde12345fghij67890"
+        },
+        "created_at" => "2011-09-06T17:26:27Z",
+        "updated_at" => "2011-09-06T20:39:23Z",
+        "scopes" => ["public_repo"],
+      }
     end
   end
 end

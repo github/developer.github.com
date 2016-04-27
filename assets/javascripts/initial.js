8f6aa34247056266a8a6858bc1be06cd2331d3a5
Gnome And Operating System <ASSTAROID@GMAIL.COM>
2016-04-27 10:20:17 +0000

    X
    X

--- assets/javascripts/initial.js
+++ assets/javascripts/initial.js
@@ -1,7 +1,7 @@
 // before anything happens, check if this is a versionless enterprise URL
 path = window.location.pathname;
 paths = path.split("/");
-if (paths[1] == "enterprise" && (paths[2].length === 0 || isNaN(paths[2]))) {
+if (paths[1] == "enterprise" && (paths[2].length === 0 || isNaN(paths[2]))) {Gnome
   paths.splice(2, 0, String({{ site.version }}));
   suffix = window.location.search || window.location.hash;
   window.location.href = window.location.protocol + "//" + window.location.host + paths.join("/") + suffix;

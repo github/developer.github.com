767476678421e4a13bfb25e1d832f3d7b4f626f4
Hubot <cwanstrath+hubot@gmail.com>
2016-06-08 10:01:53 -0700

    Sync changes from upstream repository

--- lib/responses/reactions.rb
+++ lib/responses/reactions.rb
@@ -4,19 +4,20 @@ module GitHub
   module Resources
     module Responses
       REACTION ||= {
-        "id": 1,
+        "id" => 1,
         "user" => USER,
-        "content": "heart"
+        "content" => "heart",
+        "created_at" => "2016-05-20T20:09:31Z"
       }
 
       REACTION_SUMMARY ||= {
-        "total_count": 5,
-        "+1": 3,
-        "-1": 1,
-        "laugh": 0,
-        "confused": 0,
-        "heart": 1,
-        "hooray": 0
+        "total_count" => 5,
+        "+1" =>  3,
+        "-1" => 1,
+        "laugh" =>  0,
+        "confused" => 0,
+        "heart" => 1,
+        "hooray" => 0
       }
     end
   end

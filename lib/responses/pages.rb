5dd1422bb03af0b773ea28d973b39a11751646f6
Hubot <cwanstrath+hubot@gmail.com>
2016-07-06 12:17:31 -0700

    Sync changes from upstream repository

--- lib/responses/pages.rb
+++ lib/responses/pages.rb
@@ -22,6 +22,11 @@ module GitHub
         "created_at" => "2014-02-10T19:00:49Z",
         "updated_at" => "2014-02-10T19:00:51Z"
       }
+
+      PAGES_REQUEST_BUILD ||= {
+        "url" => "https://api.github.com/repos/github/developer.github.com/pages/builds/latest",
+        "status" => "queued"
+      }
     end
   end
 end
